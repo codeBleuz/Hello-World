d8142aad7dd0752cf5b944a06dfd0c547fa37ffb
codeBleuz <34819721+codeBleuz@users.noreply.github.com>
2023-02-22 13:34:04 -0600

    Update script.js

--- Resume/script.js
+++ Resume/script.js
@@ -2,7 +2,7 @@
 onload=()=>{
   console.log ("The page is fully loaded.");
   myMessage = confirm ("The page is fully loaded");
-  setTimeout(myMessage, 5000);
-
+  setTimeout(myMessage, 1000);
+}
 
 
